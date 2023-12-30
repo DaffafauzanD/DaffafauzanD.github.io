fec42063904b73964c7661f5d364858fe4e7b521
macbookair <daffafauzan24@gmail.com>
2023-12-31 04:43:20 +0700

    update css and js

--- script/script.js
+++ script/script.js
@@ -2,3 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
+
+let btn1 = document.getElementById("btn1")
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById("btn3")
