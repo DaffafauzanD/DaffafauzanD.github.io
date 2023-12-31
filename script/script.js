ba4cf446b1e8091f469c6d9c0d6eaa131a9a4c69
macbookair <daffafauzan24@gmail.com>
2023-12-31 16:23:00 +0700

    update js and add hidden button

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
   });
 
+  const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 let btn1 = document.getElementById("btn1")
 let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
